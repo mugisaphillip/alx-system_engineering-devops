d5f650d92d883d2e9e2d7cc8285b9f556c81119f
phillip mugisawq <phillipmugisa@icloud.com>
2024-06-23 01:20:37 +0300

    web stach debugging 3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Create manifest to fix all termintion of php.
+
+exec { 'fix_phpp':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
+}
